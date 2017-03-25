4c3cc57578fb0cf448b84ba5fb12a363e7bd2edc
changriver <changriver@suboat.com>
2017-03-25 11:43:36 +0800

    add article profile favorite

--- models/User.js
+++ models/User.js
@@ -23,6 +23,8 @@ const UserSchema = new mongoose.Schema({
   },
   bio: String,
   image: String,
+  favorites: [{type: mongoose.Schema.Types.ObjectId, ref: 'Article'}],
+  following: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
   hash: String,
   salt: String
 }, {timestamps: true});
@@ -57,6 +59,34 @@ UserSchema.methods.toAuthJSON = function () {
     email: this.email,
     token: this.generateJWT()
   }
+};
+
+UserSchema.methods.toProfileJSONFor = function (user) {
+  return {
+    username: this.username,
+    bio: this.bio,
+    image: this.image,
+    following: false
+  };
+};
+
+UserSchema.methods.favorite = function (id) {
+  if (this.favorites.indexOf(id) === -1) {
+    this.favorites.push(id);
+  }
+
+  return this.save();
+};
+
+UserSchema.methods.unfavorite = function (id) {
+  this.favorites.remove(id);
+  return this.save();
+};
+
+UserSchema.methods.isFavorite = function (id) {
+  return this.favorites.some(function (favoriteId) {
+    return favoriteId.toString() === id.toString();
+  });
 };
 
 mongoose.model('User', UserSchema);
